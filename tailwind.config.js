826d38a75d7ff6858bd51272cac8ca0663616352
pan77775 <pan7775@gmail.com>
2025-04-11 18:38:18 +0800

    修正樣式問題

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Noto Sans TC"', "system-ui", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-};
+}
